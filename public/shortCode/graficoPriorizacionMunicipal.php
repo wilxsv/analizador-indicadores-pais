1418d71eba74c5bdc4ce5177e70fccf39244b7d1
wilxsv <wixl.sv@gmail.com>
2017-10-10 06:16:28 -0600

    agregado el nuevo esquema de datos

--- public/shortCode/graficoPriorizacionMunicipal.php
+++ public/shortCode/graficoPriorizacionMunicipal.php
@@ -10,21 +10,20 @@
 
 
 global $wpdb;
-/*
-$municipios = "SELECT arma, COUNT(*) as total FROM delitos WHERE delito = '".$delito."' AND anyo = '".$datos_atts['anyo']."' GROUP BY arma ORDER BY arma";
+
+$municipios = "SELECT departamento, municipio FROM `ind_municipio` group by departamento, municipio order by departamento, municipio";
  
- $qArma=$wpdb->get_results( $queryArma );
+ $qArma=$wpdb->get_results( $municipios );
  
  $series = '';
  $series.= "{ name:'$delito', type:'pie', radius : '55%', center: ['50%', '60%'], data:[";
  foreach ($qArma as $l) {
-  $categoria.= "'$l->arma',";
-  $series.= "{value:$l->total, name:'$l->arma'},"; 
+  $categoria.= "<option value=''>$l->departamento - $l->municipio</option>";
  }
-*/
 ?>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" integrity="sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ==" crossorigin=""/>
 <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js" integrity="sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log==" crossorigin=""></script>
+
 <style>
 #map {
 	width: 600px;
@@ -36,11 +35,7 @@ $municipios = "SELECT arma, COUNT(*) as total FROM delitos WHERE delito = '".$de
 	<div class="pad group">
 		<div class="grid one-third ">
 			  <p>Municipio:
-				<select>
-					  <option value="volvo">San Salvador</option>
-					  <option value="saab">San Miguel</option>
-					  <option value="mercedes">Soyapango</option>
-					  <option value="audi">Mejicanos</option>
+				<select><?php echo $categoria; ?>
 				</select> 
 			  </p>
 			  <p>Fase del PESS:
@@ -48,8 +43,8 @@ $municipios = "SELECT arma, COUNT(*) as total FROM delitos WHERE delito = '".$de
 				</select> 
 			  </p>
 			  <p>Valor del indice entre:
-				Min: </>
-				Max: </>  
+				Min: <input id="min" type="number"><br/>
+				Max: <input id="min" type="number"><br/>  
 			  </p>
 		</div>
 		<div class="grid one-third last">
@@ -62,14 +57,9 @@ $municipios = "SELECT arma, COUNT(*) as total FROM delitos WHERE delito = '".$de
  <div class="col-md-12">
 
  </div>
- <div class="col-md-4">
-	 <div class="col-md-6">Estadisticas</div>
-	 <div class="col-md-6">Mapas</div>
- </div>
- <div class="col-md-8">
-	 
- </div>
 </div>
+
+
 <script>
 	var cities = L.layerGroup();
 
@@ -106,14 +96,27 @@ $municipios = "SELECT arma, COUNT(*) as total FROM delitos WHERE delito = '".$de
 </script>
 
 <div class="row">
- <table class="table table-bordered">
+ <table class="table table-bordered display" id="datos">
   <thead>
                     <tr>
                       <th>Departamento</th>
                       <th>Municipio</th>
                       <th>Homocidio</th>
-                      <th>Suicidio</th>
-                      <th>A. Transito</th>
+                      <th>Homicidios mujeres</th>
+                      <th>Desaparecidos</th>
+                      <th>Lesiones</th>
+                      <th>viff</th>
+                      <th>extorciones</th>
+                      <th>robo</th>
+                      <th>hurto</th>
+                      <th>Robo de vehiculos</th>
+                      <th>Hurto vehiculos</th>
+                      <th>r_h_conmercio</th>
+                      <th>ppl</th>
+                      <th>ppurb</th>
+                      <th>epp</th>
+                      <th>Desaparecidos</th>
+                      <th>ipn</th>
                     </tr>
   </thead>
   <tbody>
@@ -136,3 +139,14 @@ $municipios = "SELECT arma, COUNT(*) as total FROM delitos WHERE delito = '".$de
 <?php
  include(plugin_dir_path( __FILE__ )."../footer_public.php");
 ?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo plugin_dir_url( __FILE__ ); ?>../plugins/bower_components/datatables/jquery.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
+  
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
+<script>
+$(document).ready( function () {
+    $.noConflict();
+    $('#datos').DataTable();
+} );
+</script>
